52c2c4b20b1cecf5f45c3ca3b670dfb3f92ab762
Mark Ledwich <mark@ledwich.com.au>
2020-10-28 10:48:42 +1000

    purpose box - styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,9 @@ import AboutImage from '../images/ttube-about.jpg'
 const PurposeDiv = styled.div`
   background-color:var(--bg-feature);
   color:#eee;
+  p {
+    color: #eee;
+  }
   padding:1.5em;
   font-size:1.5em;
   margin-bottom:1em;
@@ -22,6 +25,13 @@ const PurposeDiv = styled.div`
     font-weight: 300;
     margin-bottom: 1em;
   }
+
+
+
+  p.subtle {
+    font-size: 0.8em;
+    color:#ccc;
+  }
 `
 
 const IndexPage = () => {
@@ -31,9 +41,12 @@ const IndexPage = () => {
       <SEO title="transparency.tube" image={AboutImage} />
       <PurposeDiv>
         <h2>A Window into Culture and Politics on YouTube</h2>
-        YouTube is used by 71% of Americans and a source of news for 26% of US adults. While the impact of YouTube continues to grow, options for understanding the content and ideas being shared on the platform are lacking. That is why we built Transparency.tube. In creating a first-of-its-kind effort to categorize, index, and analyze over 8,000 of the largest English language YouTube channels actively discussing political and cultural issues, we aim to provide the data necessary to better understand this space.
-        
-        Note - Channel classification is based on it's content, not the identity of the channel creator. There is human judgement, and algorithmic inaccuracy so please contact us if you think a channel is missing or has been misclassified.
+
+        <p style={{ marginBottom: '1em' }}>
+          YouTube is used by 71% of Americans and a source of news for 26% of US adults. While the impact of YouTube continues to grow, options for understanding the content and ideas being shared on the platform are lacking. That is why we built Transparency.tube. In creating a first-of-its-kind effort to categorize, index, and analyze over 8,000 of the largest English language YouTube channels actively discussing political and cultural issues, we aim to provide the data necessary to better understand this space.
+        </p>
+
+        <p className="subtle">Note - Channel classification is based on it's content, not the identity of the channel creator. There is human judgement, and algorithmic inaccuracy so please contact us if you think a channel is missing or has been misclassified.</p>
       </PurposeDiv>
       <ChannelVideoViewsPage />
       <Footer />
