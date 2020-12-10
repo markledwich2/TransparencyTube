f3a1162ad5e98fea39b95e06459ba2be37182e44
Mark Ledwich <mark@ledwich.com.au>
2020-12-10 21:51:32 +1000

    gatsby build error

--- src/pages/personalization.tsx
+++ src/pages/personalization.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Layout from '../components/Layout'
 import PurposeBanner from '../components/PurposeBanner'
 
-
 const PersonalizationPage = () => <Layout>
   <PurposeBanner>
     <p>YouTube's recommended videos are tailored for each user taking into account watch history. We created 15 personas, each with their own watch history to see how YouTube's personalization works. </p>
@@ -12,3 +11,5 @@ const PersonalizationPage = () => <Layout>
     TODO: venn diagram for a hand picked collection of videos
   </div>
 </Layout>
+
+export default PersonalizationPage
